8ff7d922f33aced6b5b0e673909ea417e0ccafc1
LyangHiga <lyang_higa@poli.ufrj.br>
2019-05-14 17:44:49 -0300

    SVM Sensitivity to Feature Scales

---
+++ svm/stfs.py
@@ -0,0 +1,71 @@
+# Common imports
+import numpy as np
+import os
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+
+np.random.seed(42)
+
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+
+def save_fig(fig_id, tight_layout=True):
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(fig_id + ".png", format='png', dpi=300)
+
+def plot_svc_decision_boundary(svm_clf, xmin, xmax):
+    w = svm_clf.coef_[0]
+    b = svm_clf.intercept_[0]
+
+    # At the decision boundary, w0*x0 + w1*x1 + b = 0
+    # => x1 = -w0/w1 * x0 - b/w1
+    x0 = np.linspace(xmin, xmax, 200)
+    decision_boundary = -w[0]/w[1] * x0 - b/w[1]
+
+    margin = 1/w[1]
+    gutter_up = decision_boundary + margin
+    gutter_down = decision_boundary - margin
+
+    svs = svm_clf.support_vectors_
+    plt.scatter(svs[:, 0], svs[:, 1], s=180, facecolors='#FFAAAA')
+    plt.plot(x0, decision_boundary, "k-", linewidth=2)
+    plt.plot(x0, gutter_up, "k--", linewidth=2)
+    plt.plot(x0, gutter_down, "k--", linewidth=2)
+
+
+Xs = np.array([[1, 50], [5, 20], [3, 80], [5, 60]]).astype(np.float64)
+ys = np.array([0, 0, 1, 1])
+svm_clf = SVC(kernel="linear", C=100)
+svm_clf.fit(Xs, ys)
+
+plt.figure(figsize=(12,3.2))
+plt.subplot(121)
+plt.plot(Xs[:, 0][ys==1], Xs[:, 1][ys==1], "bo")
+plt.plot(Xs[:, 0][ys==0], Xs[:, 1][ys==0], "ms")
+plot_svc_decision_boundary(svm_clf, 0, 6)
+plt.xlabel("$x_0$", fontsize=20)
+plt.ylabel("$x_1$  ", fontsize=20, rotation=0)
+plt.title("Unscaled", fontsize=16)
+plt.axis([0, 6, 0, 90])
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(Xs)
+svm_clf.fit(X_scaled, ys)
+
+plt.subplot(122)
+plt.plot(X_scaled[:, 0][ys==1], X_scaled[:, 1][ys==1], "bo")
+plt.plot(X_scaled[:, 0][ys==0], X_scaled[:, 1][ys==0], "ms")
+plot_svc_decision_boundary(svm_clf, -2, 2)
+plt.xlabel("$x_0$", fontsize=20)
+plt.title("Scaled", fontsize=16)
+plt.axis([-2, 2, -2, 2])
+
+save_fig("sensitivity_to_feature_scales_plot")
